de2211297a3448b7086b54553c3bde897c4d4fdc
Carrie Niziolek <carrien@bu.edu>
2016-01-27 22:23:50 -0500

    Reformat speechDetector.js

--- speechDetector.js
+++ speechDetector.js
@@ -1,21 +1,24 @@
-function speechDetector(THRESHOLD, DURATION, OFFDURATION, onUtterance){
+function speechDetector(THRESHOLD, DURATION, OFFDURATION, onUtterance) {
 
   navigator.getUserMedia = (
     navigator.getUserMedia ||
-      navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia ||
-          navigator.msGetUserMedia
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia
   );
 
   var audioCtx = new AudioContext();
   var scriptNode = audioCtx.createScriptProcessor(4096, 1, 1);
 
-  navigator.getUserMedia({audio:true}, success, function(e) {
+  navigator.getUserMedia({
+    audio: true
+  }, success, function(e) {
     alert('Error capturing audio.');
   });
 
   var source = null;
-  function success(e){
+
+  function success(e) {
     source = audioCtx.createMediaStreamSource(e);
     source.connect(scriptNode);
     source.onended = function() {
@@ -26,17 +29,22 @@ function speechDetector(THRESHOLD, DURATION, OFFDURATION, onUtterance){
   }
 
   scriptNode.connect(audioCtx.destination);
-  var onSince = -1, offSince = -1, lastCheck = -1, wasOn = false;
+  var onSince = -1,
+    offSince = -1,
+    lastCheck = -1,
+    wasOn = false;
 
   // Give the node a function to process audio events
   scriptNode.onaudioprocess = function(audioProcessingEvent) {
     var inputBuffer = audioProcessingEvent.inputBuffer;
     var outputBuffer = audioProcessingEvent.outputBuffer;
     var data = inputBuffer.getChannelData(0);
-    var sum = data.reduce(function(a,b){return a+Math.abs(b);}, 0);
+    var sum = data.reduce(function(a, b) {
+      return a + Math.abs(b);
+    }, 0);
     var now = new Date().getTime();
-    if (sum > THRESHOLD){
-      if (onSince < 0){
+    if (sum > THRESHOLD) {
+      if (onSince < 0) {
         onSince = now;
         uttStartTime = now;
       }
@@ -46,18 +54,18 @@ function speechDetector(THRESHOLD, DURATION, OFFDURATION, onUtterance){
     } else {
       //console.log("OFF", onSince, now)
       if ((onSince > 0) && (now - onSince) > DURATION) {
-            wasOn = true;
+        wasOn = true;
       }
       if (wasOn && (offSince > 0) && (now - offSince) > OFFDURATION) {
-            console.log("Utterance Done", now, onSince, now-onSince)
-            onUtterance(uttStartTime, now-uttStartTime);
-            wasOn = false;
-       }
+        console.log("Utterance Done", now, onSince, now - onSince)
+        onUtterance(uttStartTime, now - uttStartTime);
+        wasOn = false;
+      }
 
-       onSince = -1;
-       if (offSince < 0) {
-         offSince = now;
-       }
+      onSince = -1;
+      if (offSince < 0) {
+        offSince = now;
+      }
     }
     lastCheck = now;
   }
